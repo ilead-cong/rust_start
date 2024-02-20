bbb846b096a72625e093aa119e15a10b54000d3a
ilead-cong <2119452560@qq.com>
2024-02-20 17:21:26 +0800

    rust practice hecto

---
+++ rust_practice/01.text_editor/hecto/src/main.rs
@@ -0,0 +1,9 @@
+mod editor;
+
+use editor::Editor;
+
+
+fn main() {
+    let editor = Editor {};
+    editor.run();
+}
---
+++ rust_practice/01.text_editor/readme.md
@@ -0,0 +1,5 @@
+# hecto
+
+ref: https://www.flenker.blog/hecto/
+
+20240220: https://www.flenker.blog/hecto-chapter-3/
